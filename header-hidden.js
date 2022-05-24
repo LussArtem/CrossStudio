bf888686baab8dc5e916a42f9152d44cb4981293
Artem L <vlossaa@gmail.com>
2022-05-24 23:00:33 +0700

    fix: fix some style && debug header

--- header-hidden.js
+++ header-hidden.js
@@ -1,43 +1,69 @@
-// window.addEventListener('scroll',(event) => { 
-//     let scrollTop = window.scrollHeight;
-//     // console.log('2');
-//     event
-//     let headerWrapper = document.querySelector('#jsHeader');
-//     if(scrollTop >= 100){
-//         headerWrapper.classList.add('_hide');
-//         // console.log('2');
-//     }else{    
-//         headerWrapper.classList.remove('_hide');
-//         // console.log('3');
-//     }
-// });
-
-// window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-//     document.getElementById("jsHeader").classList.add("_shrink");
-//     document.getElementsByClassName("header__logo");
-//     console.log('2')
-//   } else {
-//     document.getElementById("jsHeader").classList.remove("_shrink");
-//   }
-// }
-
+// слушатель событий скролл
 document.addEventListener("scroll",
+// создаем функцию
 function(){
+    // обьявлем константы
     const shrinkHeader = 5;
+    const header = document.getElementById('jsHeader');
     let headerWrapper = document.querySelector('.header__logo');
+    // слушатель событий
     window.addEventListener('scroll', function() {
-       var scroll = getCurrentScroll();
-         if ( scroll >= shrinkHeader ) {
-              headerWrapper.classList.add('_shrink');
-           }
-           else {
-               headerWrapper.classList.remove('_shrink')
-           }
-     });
-   function getCurrentScroll() {
+        const scroll = getCurrentScroll();
+            if ( scroll >= shrinkHeader ) {
+                headerWrapper.classList.add('_shrink');
+                header.classList.add('_shrink');
+            }
+            else {
+                headerWrapper.classList.remove('_shrink');
+                header.classList.remove('_shrink');
+            }
+        });
+    function getCurrentScroll() {
        return window.pageYOffset || document.documentElement.scrollTop;
-       }
-   });
+    }
+});
+
+
+function fixedHeader(header, rootElement = document.body) {
+    if (!header) throw new Error('No header found!');
+    if (!rootElement) rootElement = document.body;
+    
+    header.classList.add('fl-fixed-header');
+    console.log('1');
+    function setPadding() {
+        rootElement.style.paddingTop = `${header.offsetHeight}px`;
+        console.log('1');
+    }
+    window.addEventListener('resize', _.debounce(setPadding, 250));
+    setPadding();
+}
+
+function scrollUpHeader(header, threshold = 10) {
+    let scrollPos = 0;
+    console.log('1');
+
+    function direction() {
+        const bodyTop = document.body.getBoundingClientRect().top;
+        if (Math.abs(bodyTop - scrollPos) <= threshold) {
+            // Less than threshold
+            scrollPos = bodyTop;
+            return;
+            
+        }
+        if (bodyTop > scrollPos) {
+            // Up
+            header.classList.remove('hide');
+        } else if (window.scrollY > header.offsetHeight) {
+            // Down
+            header.classList.add('hide');
+        }
+        scrollPos = bodyTop;
+    }
+    window.addEventListener('scroll', _.throttle(direction, 250));
+}
+
+window.onload = () => {
+    const header = document.querySelector('#jsHeader');
+    fixedHeader(header);
+    scrollUpHeader(header);
+};
