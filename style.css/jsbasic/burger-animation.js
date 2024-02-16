27e1e0a920096897b62031eb7239bc6be10231fb
Artem L <vlossaa@gmail.com>
2024-02-16 18:56:08 +0700

    fix: "some fix"

--- style.css/jsbasic/burger-animation.js
+++ style.css/jsbasic/burger-animation.js
@@ -1,8 +1,9 @@
-const burgerMenu = document.querySelector(".burger__item");
+// const burgerMenu = document.querySelector(".burger__item");
+const burgerMenu = document.querySelector(".burger");
 const header = document.querySelector("#jsHeader");
 const navItem = document.querySelector("nav");
 if (burgerMenu) {
-    burgerMenu.addEventListener("click", function(e) {
+    burgerMenu.addEventListener("click", function (e) {
         document.body.classList.toggle("_hidden");
         burgerMenu.classList.toggle("_active");
         navItem.classList.toggle("_active");
--- style.css/jsbasic/nav-smoothly.js
+++ style.css/jsbasic/nav-smoothly.js
@@ -1,40 +1,41 @@
 // Плавная прокрутка при клике
 // 1.Собираем массив объектов для навигации
-const menuLinks = document.querySelectorAll('.nav__link[data-goto]');
-
+// const menuLink1 = document.querySelectorAll(".nav__link[data-goto]");
+const menuLinks = document.querySelectorAll("[data-goto]");
+// console.log(menuLink1);
 // 2. Проверка (есть ли такие элементы)
 if (menuLinks.length > 0) {
-
     // 3. перебираем элементы с помощью форыча
-    menuLinks.forEach(menuLink => {
-
-        // 4. Добавляем событие клик (сразу идем в функцию) 
+    menuLinks.forEach((menuLink) => {
+        // 4. Добавляем событие клик (сразу идем в функцию)
         menuLink.addEventListener("click", oneMenuLinkClick);
     });
     // 5. Тут мы создаем функцию
     function oneMenuLinkClick(e) {
-
         // 6. Получаем обьект на который мы кликаем
         const menuLink = e.target;
 
         //  7. Делаем условие (заполнен ли датаАтрибут И существует ли объект)
-        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-
+        if (
+            menuLink.dataset.goto &&
+            document.querySelector(menuLink.dataset.goto)
+        ) {
             // 8. Получаем в константу сам объект
             const gotoBlock = document.querySelector(menuLink.dataset.goto);
 
             // // Высчитываем положение объекта относительно высоты шапки 83
             // const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
 
-            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - 83;
+            const gotoBlockValue =
+                gotoBlock.getBoundingClientRect().top + scrollY - 83;
 
             // // Закрываем Бургер меню на мобиле при прокрутке
             // //  если имеет класс, то
-            if (burgerMenu.classList.contains('_active')) {
-                document.body.classList.remove('_hidden');
-                burgerMenu.classList.remove('_active');
-                navItem.classList.remove('_active');
-                header.classList.toggle('_active');
+            if (burgerMenu.classList.contains("_active")) {
+                document.body.classList.remove("_hidden");
+                burgerMenu.classList.remove("_active");
+                navItem.classList.remove("_active");
+                header.classList.toggle("_active");
                 // navItem.style.display = "block";
             }
 
@@ -44,7 +45,7 @@ if (menuLinks.length > 0) {
                 top: gotoBlockValue,
 
                 //  Прокрутка плавная
-                behavior: "smooth"
+                behavior: "smooth",
             });
 
             //  Отключаем работу ссылки
